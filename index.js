b6381a6ecdd9d450d9c5ef4e4b669a2635d01c6a
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-31 11:40:18 -0700

    Added Health Bar Interface

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -157,7 +158,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        console.log('Player attack successful')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (
@@ -168,7 +170,8 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log('Enemy attack successful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
