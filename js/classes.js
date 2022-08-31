dbded9b34521c30afb56944c19e82e3773fe3146
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-31 14:03:37 -0700

    Added animation to shop Sprite

--- js/classes.js
+++ js/classes.js
@@ -1,25 +1,44 @@
 class Sprite {
-    constructor({ position, imageSrc, scale = 1}) {
+    constructor({ position, imageSrc, scale = 1, framesMax = 1}) {
         this.position = position
         this.width = 50
         this.height = 150
         this.image = new Image()
         this.image.src = imageSrc
         this.scale = scale
+        this.framesMax = framesMax
+        this.frameCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
     }
 
     draw() {
         c.drawImage(
             this.image,
+            this.frameCurrent * (this.image.width / this.framesMax),
+            0,
+            this.image.width / this.framesMax, //6 refers to the amount of frames in the actual image
+            this.image.height,
             this.position.x,
             this.position.y,
-            this.image.width * this.scale,
+            (this.image.width / this.framesMax) * this.scale,
             this.image.height * this.scale
         )
     }
 
     update() {
         this.draw()
+        this.framesElapsed++
+
+        if (this.framesElapsed % this.framesHold === 0) {
+           if (this.frameCurrent < this.framesMax - 1) {
+            this.frameCurrent++
+            } else {
+            this.frameCurrent = 0
+            } 
+        }
+        
+        
     }
 }
 
